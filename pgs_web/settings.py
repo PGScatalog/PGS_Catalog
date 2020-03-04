05aa989dea3e3b0c7db43f8d7a5d58e7326a200c
Laurent Gil <lg10@sanger.ac.uk>
2020-03-04 14:04:48 +0000

    Update homepage and templates settings

--- pgs_web/settings.py
+++ pgs_web/settings.py
@@ -69,22 +69,45 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pgs_web.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'catalog.context_processors.pgs_urls'
-            ],
+if DEBUG == True:
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                    'catalog.context_processors.pgs_urls'
+                ],
+            },
         },
-    },
-]
+    ]
+else:
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(BASE_DIR, 'templates')],
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                    'catalog.context_processors.pgs_urls'
+                ],
+                'loaders': [
+                    ('django.template.loaders.cached.Loader', [
+                        'django.template.loaders.filesystem.Loader',
+                        'django.template.loaders.app_directories.Loader'
+                    ])
+                ]
+            },
+        },
+    ]
 
 USEFUL_URLS = {
     'BAKER_URL'      : 'https://baker.edu.au',
