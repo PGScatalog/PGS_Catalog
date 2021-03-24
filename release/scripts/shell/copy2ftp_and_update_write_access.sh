52bb53b90f31f533a6e825a2a72819f3c78a9664
Laurent Gil <lg10@sanger.ac.uk>
2021-03-24 10:49:56 +0000

    Simplify a bit the release process and update the website with the new paper

---
+++ release/scripts/shell/copy2ftp_and_update_write_access.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "This script requires 2 parameters: <local_data_dir> and <ftp_dir>"
+else
+  local_data_dir=$1
+  ftp_dir=$2
+
+  if [ -d ${ftp_dir} ]; then
+
+    if [ -d ${local_data_dir} ]; then
+      # Copy data files to FTP
+      echo "> copy to FTP - started"
+      cp -R  ${local_data_dir}* ${ftp_dir}
+      echo "> copy to FTP - done"
+
+      # Update write access on FTP
+      cd ${ftp_dir}
+      find pgs_scores_list.txt -type f -exec chmod 664 {} +
+      echo "> pgs_scores_list.txt file - done"
+      find scores/ -type d -exec chmod 775 {} +
+      echo "> scores directories - done"
+      find scores/ -type f -exec chmod 664 {} +
+      echo "> scores files - done"
+      find metadata/ -type d -exec chmod 775 {} +
+      echo "> metadata directories - done"
+      find metadata/ -type f -exec chmod 664 {} +
+      echo "> metadata files - done"
+    else
+      echo "Directory '${local_data_dir}' can't be found"
+    fi
+  else
+    echo "Directory '${ftp_dir}' can't be found"
+  fi
+fi
