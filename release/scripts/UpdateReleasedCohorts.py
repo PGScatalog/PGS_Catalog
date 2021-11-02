0d4c87fca7655c7db8cf9ce5be94eb1616390d85
Laurent Gil <lg10@sanger.ac.uk>
2021-11-02 15:36:24 +0000

    Optimise the UpdateReleasedCohorts release script

--- release/scripts/UpdateReleasedCohorts.py
+++ release/scripts/UpdateReleasedCohorts.py
@@ -2,45 +2,40 @@ from catalog.models import Cohort, Score
 from pgs_web import constants
 
 class UpdateReleasedCohorts:
+    cohorts_released = []
+    cohorts_not_released = []
 
     def __init__(self):
-        self.cohorts = Cohort.objects.all()
+        self.cohorts = Cohort.objects.only('id','released').all()
         self.score_id_list = Score.objects.values_list('id', flat=True).filter(date_released__isnull=False)
 
     def update_cohorts(self):
         count_all = 0
-        count_released = 0
-        count_not_released = 0
         for cohort in self.cohorts:
             c_associations = cohort.associated_pgs_ids
+            associations_list = set( c_associations['development'] + c_associations['evaluation'] )
+
             released = False
-            for score in c_associations['development']:
-                if score in self.score_id_list:
-                    released = True
-                    break
+            if (set(self.score_id_list).intersection(associations_list)):
+                released = True
 
             if released == False:
-                for score in c_associations['evaluation']:
-                    if score in self.score_id_list:
-                        released = True
-                        break
-            count_all += 1
-            if released == False:
-                count_not_released += 1
+                self.cohorts_not_released.append(cohort.id)
             else:
-                count_released += 1
-            cohort.released = released
-            cohort.save()
+                self.cohorts_released.append(cohort.id)
+
+        # Update DB with new values
+        Cohort.objects.filter(id__in=self.cohorts_released).update(released=True)
+        Cohort.objects.filter(id__in=self.cohorts_not_released).update(released=False)
 
         db_count_released = Cohort.objects.filter(released=True).count()
         db_count_not_released = Cohort.objects.filter(released=False).count()
-        print(f'> Cohorts - all: {count_all}')
-        print(f'> Cohorts - released: {count_released} | DB: {db_count_released}')
-        print(f'> Cohorts - not released: {count_not_released} | DB: {db_count_not_released}')
+        print(f'> Cohorts - all: {len(self.cohorts)}')
+        print(f'> Cohorts - released: {len(self.cohorts_released)} | DB: {db_count_released}')
+        print(f'> Cohorts - not released: {len(self.cohorts_not_released)} | DB: {db_count_not_released}')
 
 
 def run():
     """ Update the Cohort entries, setting the flag 'released'."""
-
     released_cohorts = UpdateReleasedCohorts()
     released_cohorts.update_cohorts()
