1dd1f4f7612fc810c07ac9f7e0a631a55cfa254b
Florent Yvon <florent.yvon76@gmail.com>
2024-02-13 15:06:28 +0000

    Updated metadata template link to v10

--- pgs_web/constants.py
+++ pgs_web/constants.py
@@ -26,7 +26,7 @@ USEFUL_URLS = {
     'PGS_WEBSITE_URL'   : 'https://www.pgscatalog.org/',
     'UOC_URL'           : 'https://www.phpc.cam.ac.uk/',
     'TERMS_OF_USE'      : 'https://www.ebi.ac.uk/about/terms-of-use',
-    'TEMPLATEGoogleDoc_URL' : 'https://docs.google.com/spreadsheets/d/1kVhi4GhXUVCl5THgbrmq2oiegjFFeuY3/edit?usp=sharing',
+    'TEMPLATEGoogleDoc_URL' : 'https://docs.google.com/spreadsheets/d/1UEGH0NNuQ8ifbsxIhe8HbnG9XYjsIwSF/edit?usp=sharing',
     'CurationGoogleDoc_URL' : 'https://drive.google.com/file/d/1iYoa0R3um7PtyfVO37itlGbK1emoZmD-/view',
     'CATALOG_PUBLICATION_URL' : 'https://doi.org/'+PGS_CITATION['doi'],
     'EPMC_REST_SEARCH'  : 'https://www.ebi.ac.uk/europepmc/webservices/rest/search'
