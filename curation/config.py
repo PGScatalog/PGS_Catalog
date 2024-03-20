fb48ad20e8e56f05ea2c2bdcadeb933f98691aa0
Florent Yvon <florent.yvon76@gmail.com>
2024-03-20 16:40:45 +0000

    Restored default config values

--- curation/config.py
+++ curation/config.py
@@ -1,25 +1,25 @@
 curation_directories = {
     'template_schema': './curation/templates/TemplateColumns2Models.xlsx',
     'scoring_schema': './curation/templates/ScoringFileSchema.xlsx',
-    'studies_dir': '/home/florent/PGS_Catalog/Releases/Import_Next',
-    'scoring_dir': '/home/florent/PGS_Catalog/Releases/Import_Next/ScoringFiles/'
+    'studies_dir': '<studies_dir_path>',
+    'scoring_dir': '<studies_dir_path>/ScoringFiles/'
 }
 
 study_names_list = [
-    {'name': 'Truong2024_AuthorSub'}
+    {'name': '<study_name>'},
 ]
 
 default_curation_status = 'IP'
 
 scoringfiles_format_version = '2.0'
 
-skip_scoringfiles = True
+skip_scoringfiles = False
 
-skip_curationtracker = True
+skip_curationtracker = False
 
 variant_positions_qc_config = {
-    'skip': True,  # Set to True to ignore the variant positions QC step
-    'n_requests': 1,  # Maximum number of requests allowed per score to the Ensembl REST API
+    'skip': False,  # Set to True to ignore the variant positions QC step
+    'n_requests': 4,  # Maximum number of requests allowed per score to the Ensembl REST API
     'ensembl_max_variation_req_size': 10,  # Maximum number of variants per request to the Ensembl variation REST API
     'ensembl_max_sequence_req_size': 50,  # Maximum number of variants per request to the Ensembl sequence REST API
     'minimum_match_rate': 0.9  # The minimum required match rate for the QC to be satisfactory
