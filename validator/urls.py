9b6416b12b7429e13be474f90d7c5c85530c1bad
Florent Yvon <florent.yvon76@gmail.com>
2024-10-22 17:38:19 +0100

    Moved score validator to labs

--- validator/urls.py
+++ validator/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("validate_metadata_client/", views.validate_metadata_template_client, name="Metadata Template Validation"),
-    path("validate_scoring_files_client/", views.validate_scoring_files_client, name="Scoring Files Validation"),
+    path("labs/validate_scoring_files_client/", views.validate_scoring_files_client, name="Scoring Files Validation"),
 ]
