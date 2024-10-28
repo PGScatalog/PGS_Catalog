7cccb4e08675b6480c1b6ed24e6f66aae5f56cdd
Florent Yvon <florent.yvon76@gmail.com>
2024-10-28 15:00:55 +0000

    Removed "_client" suffix from validators urls

--- validator/urls.py
+++ validator/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("validate_metadata_client/", views.validate_metadata_template_client, name="Metadata Template Validation"),
-    path("labs/validate_scoring_files_client/", views.validate_scoring_files_client, name="Scoring Files Validation"),
+    path("validate_metadata/", views.validate_metadata_template_client, name="metadata_template_validation"),
+    path("labs/validate_scoring_files/", views.validate_scoring_files_client, name="scoring_files_validation"),
 ]
