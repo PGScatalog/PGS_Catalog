8698b318204f5a4b349129db773ce19fa1c5e86a
Florent Yvon <florent.yvon76@gmail.com>
2025-01-16 14:25:49 +0000

    Updated font-awesome version
    Needed for social media links update (X and bluesky)

--- pgs_web/external_urls.py
+++ pgs_web/external_urls.py
@@ -1,7 +1,7 @@
 STYLES_URLS = {
     'bootstrap': 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css',
     'bootstrap-table': 'https://unpkg.com/bootstrap-table@1.21.3/dist/bootstrap-table.min.css',
-    'font-awesome': 'https://use.fontawesome.com/releases/v6.3.0/css/all.css',
+    'font-awesome': 'https://use.fontawesome.com/releases/v6.7.2/css/all.css',
     'jquery': 'https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css',
     'ebi': 'https://ebi.emblstatic.net/web_guidelines/EBI-Icon-fonts/v1.3/fonts.css'
 }
