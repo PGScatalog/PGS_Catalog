c149c7c39eb864bf378d7a843c2aad12fa7909c8
Florent Yvon <florent.yvon76@gmail.com>
2025-03-17 15:14:45 +0000

    Added support for custom list of scores for partial copy

--- release/scripts/run_copy_scoring_files.py
+++ release/scripts/run_copy_scoring_files.py
@@ -5,17 +5,18 @@ from release.scripts.CopyScoringFiles import CopyScoringFiles
 from release.scripts.CopyHarmonizedScoringFilesPOS import CopyHarmonizedScoringFilesPOS
 
 
-def copy_scoring_files(new_ftp_dir,staged_scores_dir,scores_dir,md5_sql_filepath,username):
+def copy_scoring_files(new_ftp_dir, staged_scores_dir, scores_dir, md5_sql_filepath, username, custom_list_scores=None):
     print("\n#### Copy the new formatted scoring files ####")
-    pgs_scoring_files = CopyScoringFiles(new_ftp_dir,staged_scores_dir,scores_dir,md5_sql_filepath,username)
+    pgs_scoring_files = CopyScoringFiles(new_ftp_dir, staged_scores_dir, scores_dir, md5_sql_filepath, username, custom_list_scores)
     pgs_scoring_files.get_previous_release()
     pgs_scoring_files.get_list_of_scores()
     pgs_scoring_files.copy_scoring_files_to_production()
     pgs_scoring_files.copy_scoring_files_to_metadata()
 
-def copy_hmpos_scoring_files(new_ftp_dir,staged_scores_dir,scores_dir,md5_sql_filepath,username):
+
+def copy_hmpos_scoring_files(new_ftp_dir, staged_scores_dir, scores_dir, md5_sql_filepath, username, custom_list_scores=None):
     print("\n#### Copy the new harmonized position scoring files ####")
-    pgs_harmonized_files = CopyHarmonizedScoringFilesPOS(new_ftp_dir,staged_scores_dir,scores_dir,md5_sql_filepath,username)
+    pgs_harmonized_files = CopyHarmonizedScoringFilesPOS(new_ftp_dir, staged_scores_dir, scores_dir, md5_sql_filepath, username, custom_list_scores)
     pgs_harmonized_files.copy_harmonized_files_to_production()
     pgs_harmonized_files.copy_harmonized_files_to_metadata()
 
@@ -43,6 +44,7 @@ def main():
     argparser.add_argument("--hm_staged_scores_dir", type=str, help='The path to the harmonized Position staged files directory', required=True)
     argparser.add_argument("--hm_scores_dir", type=str, help='The path to the harmonized scoring files directory (Production)', required=False)
     argparser.add_argument("--username", type=str, help='Linux/Unix username', required=True)
+    argparser.add_argument("--list_of_scores", type=str, help='Custom list of scores for partial copy', required=False, default=None)
 
     args = argparser.parse_args()
 
@@ -52,6 +54,7 @@ def main():
     hm_staged_scores_dir = args.hm_staged_scores_dir
     hm_scores_dir = args.hm_scores_dir
     username = args.username
+    custom_list_of_scores = args.list_of_scores
 
     #release_date_file = f'{new_ftp_dir}/release_date.txt'
     #new_release_date = get_new_release_date(release_date_file)
@@ -60,9 +63,9 @@ def main():
     #md5_sql_filepath = f'{new_ftp_dir}/{md5_sql_filename}'
     md5_sql_filepath = None
 
-    copy_scoring_files(new_ftp_dir,staged_scores_dir,scores_dir,md5_sql_filepath,username)
+    copy_scoring_files(new_ftp_dir, staged_scores_dir, scores_dir, md5_sql_filepath, username, custom_list_of_scores)
 
-    copy_hmpos_scoring_files(new_ftp_dir,hm_staged_scores_dir,hm_scores_dir,md5_sql_filepath,username)
+    copy_hmpos_scoring_files(new_ftp_dir, hm_staged_scores_dir, hm_scores_dir, md5_sql_filepath, username, custom_list_of_scores)
 
     # Move/remove temporary files
     # if os.path.isfile(release_date_file):
