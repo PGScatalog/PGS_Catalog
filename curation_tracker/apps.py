63dfc86a16686d77c25352b0c33c50770e303aeb
Florent Yvon <florent.yvon76@gmail.com>
2026-03-20 18:16:30 +0000

    Registering auditlog models in AppConfig.ready()
    This ensures models are registered in the curation context but not in the prod-live context

--- curation_tracker/apps.py
+++ curation_tracker/apps.py
@@ -4,3 +4,20 @@ from django.apps import AppConfig
 class CurationTrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'curation_tracker'
+
+    def ready(self):
+
+        # Registering models with auditlog for change tracking
+        # Although it would be recommended to register models in catalog/models.py, it is safer to do it here as
+        # the auditlog table in located in the curation_tracker DB
+        self.register_models_with_auditlog()
+
+    def register_models_with_auditlog(self):
+        """Registers models with auditlog for change tracking."""
+        import logging
+        logging.getLogger(__name__).info("Registering models with auditlog for change tracking.")
+        from auditlog.registry import auditlog
+        from catalog.models import Publication, Score
+
+        auditlog.register(Score, m2m_fields={"trait_efo"}, exclude_fields={"ancestries"})
+        auditlog.register(Publication)
